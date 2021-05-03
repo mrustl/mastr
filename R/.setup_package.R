83b4c7e8dcff7b5b32918f05b82bbc83d169477d
Michael Rustler <michael.rustler@kompetenz-wasser.de>
2021-05-04 01:06:54 +0200

    Create pkg with kwb.pkgbuild

---
+++ R/.setup_package.R
@@ -0,0 +1,17 @@
+pkg <- list(
+  name = "mastr",
+  title = "R Package for Visualising Data from Marktstammdatenregister (for Berlin)",
+  desc = paste(
+    "R Package for Visualising Data from Marktstammdatenregister (for Berlin)."
+  )
+)
+
+kwb.pkgbuild::use_pkg_skeleton("mastr")
+
+kwb.pkgbuild::use_pkg(
+  pkg = pkg,
+  copyright_holder = list(name = "Michael Rustler", start_year = NULL),
+  user = "mrustl"
+)
+
+kwb.pkgbuild::create_empty_branch_ghpages("mast")
